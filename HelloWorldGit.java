2b55b1966e4264cbe79a69d5c9cab90e89ef9428
jagerdown <jager.down@gmail.com>
2016-04-20 14:43:14 -0400

    Adding Java file

---
+++ HelloWorldGit.java
@@ -0,0 +1,18 @@
+public class HelloWorldGit {
+	
+	
+	public static void printStr (String str){
+		System.out.println(str);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		printStr("Hello World!");
+		printStr("What's is the weather like?");
+		printStr("Sunny!");
+
+	}
+
+}
